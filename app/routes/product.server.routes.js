ddb39b570687b29cde4922dbf66881c88bab2b6f
dtytomi <dtytomi@gmail.com>
2015-02-04 01:53:58 +0100

    clean ups'

--- app/routes/product.server.routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var product =  require('../controllers/product.server.controller.js');
-
-module.exports = function(app) {
-  app.post('/products', product.create);
-  app.get('/products', product.list);
-};
