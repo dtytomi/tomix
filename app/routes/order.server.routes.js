9a6016f6a36c86b4c9ce0d7c0d1a1caa4133b356
dtytomi <dtytomi@gmail.com>
2015-02-11 03:54:27 +0100

    product route and order route route reworked upon

--- app/routes/order.server.routes.js
+++ app/routes/order.server.routes.js
@@ -1,8 +1,17 @@
 'use strict';
 
-var orders = require('../../app/controllers/orders');
+var orders = require('../../app/controllers/orders'),
+    users = require('../../app/controllers/users');
 
 module.exports =function(app) {
-  app.route('/order')
-     .post(orders.create);
+  app.route('/cart')
+     .get(orders.list)
+     .post(users.requiresLogin, orders.addToCart);
+
+  app.route('/cart/:orderId')
+     .get(orders.read)
+     .put(users.requiresLogin, orders.hasAuthourization, orders.update)
+     .delete(users.requiresLogin, orders.hasAuthourization, orders.delete);
+
+  app.param('orderId', orders.orderById);
 };
